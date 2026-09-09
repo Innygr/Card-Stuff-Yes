4b6df526daa79d509d83ae5f82944392abc7f98d
Innes <innygrant@outlook.com>
2026-09-09 16:06:15 -0700

    Set up HTTP server with configuration

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const http = require("http");
+const config = require("./config.json");
 
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Card Stuff Yes server is running!");
+});
+
+server.listen(config.port, config.host, () => {
+    console.log(`Card Stuff Yes server running on ${config.host}:${config.port}`);
+});
